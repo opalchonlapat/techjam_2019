1ace361cbbf10f7a732802ac42e4d59cb40988aa
Chonlapat Patanajirasit <chonlapat@central.tech>
2019-11-01 11:02:58 +0700

    Add read file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Techjam 2019
 All utility functions for Techjam 2019
+
+- List of functions
+    - Split data
+    - Read files
--- utils_data_prep.py
+++ utils_data_prep.py
@@ -11,3 +11,21 @@ def split_validation(X: pd.DataFrame, y: pd.Series, num_bin: int = 50, test_size
     """
     bins = pd.cut(y, num_bin, labels = [str(i) for i in range(num_bin)])
     return train_test_split(X, y, test_size=test_size, random_state=seed, stratify=bins)
+
+def read_given_files() -> list:
+    """
+        Read given KBTG files and define data type to appropriate type
+
+        Output
+        ---------
+        demographic_df, credit_df, kplus_df, train_df
+    """
+    demographic_df = pd.read_csv('/content/drive/My Drive/techjam_2019/data_pack/demographics.csv', 
+                                dtype={'id':str, 'cc_no':str, 'gender':'category', 'ocp_cd':'category','age':'category'})
+    credit_df = pd.read_csv('/content/drive/My Drive/techjam_2019/data_pack/cc.csv',
+                            dtype={'cc_no':str}, parse_dates=['pos_dt'])
+    kplus_df = pd.read_csv('/content/drive/My Drive/techjam_2019/data_pack/kplus.csv',
+                        dtype={'id':str}, parse_dates=['sunday'])
+    train_df = pd.read_csv('/content/drive/My Drive/techjam_2019/data_pack/train.csv',
+                        dtype={'id':str})
+    return demographic_df, credit_df, kplus_df, train_df
